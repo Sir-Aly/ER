8abd1b3015a560a0291753640772b7a79362af87
mohamed321321 <mohamedhisham342000@gmail.com>
2023-04-28 08:40:31 +0300

    Choose between 2 Options page

---
+++ app/src/main/java/com/example/easyreach/Choose_between_2options.java
@@ -0,0 +1,14 @@
+package com.example.easyreach;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Choose_between_2options extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_between2options);
+    }
+}
